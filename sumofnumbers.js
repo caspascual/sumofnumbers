041fa4be7b3c74262da220109fada66e44b32f8f
Christopher Pascual <caspascual1293@gmail.com>
2023-02-03 14:14:05 -1000

    Added the four loops

--- sumofnumbers.js
+++ sumofnumbers.js
@@ -1,36 +1,37 @@
-function sumFor(arr) {
+const numbers = [1, 2, 3, 4, 5];
+
+function sumFor(nums) {
   let sum = 0;
-  for (let i = 0; i < arr.length; i++) {
-    sum += arr[i];
+  for (const num of nums) {
+    sum += num;
   }
   return sum;
 }
+console.log(sumFor(numbers));
 
-function sumWhile(arr) {
+function sumWhile(nums) {
   let sum = 0;
   let i = 0;
-  while (i < arr.length) {
-    sum += arr[i];
+  while (i < nums.length) {
+    sum += nums[i];
     i++;
   }
   return sum;
 }
+console.log(sumWhile(numbers));
 
-function sumRecursion(arr) {
-  if (arr.length === 0) {
+function sumRecursion(nums) {
+  const arr = nums;
+  if (nums.length === 0) {
     return 0;
   }
-  return arr.pop() + sumRecursion(arr);
+  return nums[0] + sumRecursion(arr.splice(1, arr.length));
+}
+console.log((sumRecursion(numbers)));
+
+function sumTheSimpleWay(nums) {
+  return _.reduce(nums, function (memo, num) { return memo + num; });
 }
 
-function sumTheSimpleWay(arr) {
-  const sum = _.reduce(arr, function (memo, num) { return memo + num; });
-  return sum;
-}
-
-const numbers = [1, 2, 3, 4, 5];
-
-console.log(sumFor(numbers));
-console.log(sumWhile(numbers));
-console.log(sumRecursion(numbers));
 console.log(sumTheSimpleWay([1, 2, 3, 4, 5]));
+
